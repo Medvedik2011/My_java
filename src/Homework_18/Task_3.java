86be3c4cf572bba972cf1e76523f3cc2e7933dfe
Mykhailo <mg12@i.ua>
2023-03-22 16:32:28 +0200

    add HW_18_Task_3

--- src/Homework_18/Task_3.java
+++ src/Homework_18/Task_3.java
@@ -1,17 +1,36 @@
 package Homework_18;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task_3 {
-    public static void main(String[] args) {
+
+        public static void main(String[] args) {
+            int[] numbers = {0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            sortNum(numbers);
+        }
+        public static void sortNum(int[] numbers) {
+            List<Integer> chNum = new ArrayList<>();
+            List<Integer> nechNum = new ArrayList<>();
+
+            for (int number : numbers) {
+                if ((number % 2) == 0) {
+                    chNum.add(number);
+                } else {
+                    nechNum.add(number);
+                }
+            }
+
+            System.out.println("Четные числа");
+            for (int number : chNum) {
+                System.out.println(number);
+            }
+
+            System.out.println("Нечетные числа");
+            for (int number : nechNum) {
+                System.out.println(number);
+            }
+        }
 
     }
-    public static void printNum(int n) {
-    if (n == 0) {
-        return;
-    }
-    int digit = n % 10;
-    System.out.print(digit);
-    System.out.print(digit);
-    printNum(n / 10);
-    System.out.print(" ");
-}
-}
