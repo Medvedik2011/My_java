a3bda71e22d93cc5354807024556fcc78bcc4ebb
Mykhailo <mg12@i.ua>
2023-03-23 15:30:15 +0200

    del

---
+++ src/Classwork/Calculator.java
@@ -0,0 +1,16 @@
+/*package Classwork;
+
+public static void main(String[]args){
+
+
+public class Calculator {
+    public static void calculat(String text){
+        System.out.println("I can`t");
+    }
+
+    public static void calculat(String text){
+        System.out.println("I can`t");
+        System.out.println("I can`t. Fuck java" + text);
+    }
+
+}*/
---
+++ src/Classwork/main.java
@@ -0,0 +1,18 @@
+package Classwork;
+
+public class main {
+    public static void main(String[] args) {
+        Computer computer=new Computer();
+        computer.setName("Apple");
+        System.out.println(computer.getName());
+        computer.name="Hren ";
+        System.out.println(computer.getName());
+
+
+
+
+        int[] a={2,3,5};
+        System.out.println(a.length);
+
+    }
+}
